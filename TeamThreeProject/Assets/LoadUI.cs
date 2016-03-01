607a7ba869af79078b108300bace066ad6b698f8
Lucidify <grimmclassic@gmail.com>
2016-03-01 16:50:39 +0000

    Signed-off-by: Lucidify <grimmclassic@gmail.com>

---
+++ TeamThreeProject/Assets/LoadUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class LoadUI : MonoBehaviour {
+
+    public GameObject[] menu;
+
+    public static List<PlayerPathStats> savedStats = new List<PlayerPathStats>();
+
+    // Use this for initialization
+    void Start () {
+        menu[1].SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (File.Exists(Application.persistentDataPath + "/savedStats.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedStats.gd", FileMode.Open);
+            LoadUI.savedStats = (List<PlayerPathStats>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+
+    public void ReturnToMenu()
+    {
+        menu[1].SetActive(false);
+        menu[0].SetActive(true);
+    }
+}
